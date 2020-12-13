292a84261980f0d7d8978e404a4b9f9d6e56d4eb
cangokceaslan <cangokceaslanx@gmail.com>
2020-12-13 20:23:41 +0300

    Some Variable Change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 var port = 3000;
 var app = express();
 var wikiUrls1 = "";
-var can = "Can Gökçeaslan";
+var cangokceaslan = "Can Gökçeaslan 12345";
 rp(url)
   .then(function (html) {
     //success!
