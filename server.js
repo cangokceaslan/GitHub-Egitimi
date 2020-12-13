6f88b6976d424d62296d50adbcb968f0825f3150
cangokceaslan <cangokceaslanx@gmail.com>
2020-12-13 20:24:44 +0300

    Some Other Variable Changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 var port = 3000;
 var app = express();
 var wikiUrls1 = "";
-var can = "Can Gökçeaslan";
+var conflictVar = "Can 123546";
 rp(url)
   .then(function (html) {
     //success!
