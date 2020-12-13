dd3c5b2b3315aa075c748244dd67838122040f86
cangokceaslan <cangokceaslanx@gmail.com>
2020-12-13 19:56:17 +0300

    Değişiklik

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 var port = 3000;
 var app = express();
 var wikiUrls1 = "";
-// cangokceaslan Branch'ine aittir
+// alsşdfşösdşlföşlsdfö şlö
 rp(url)
   .then(function (html) {
     //success!
